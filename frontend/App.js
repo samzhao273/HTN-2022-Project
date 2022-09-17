df4589fe318e5601cff398381eda9eb7299988ee
samzhao273 <samzhao273@gmail.com>
2022-09-17 01:59:51 -0400

    changed structure of front end folder

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MainApp from './MainApp/NavigationContainer/MainContainer'
+import Ionic from "react-native-vector-icons/Ionicons"
+import * as React from 'react';
+import MainContainer from './Navigation/MainContainer'
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <MainApp/>
-    </SafeAreaView>
+    <MainContainer/>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- frontend/Navigation/screens/MapScreen.js
+++ frontend/Navigation/screens/MapScreen.js
@@ -1,14 +1,12 @@
 import * as Reach from 'react';
 import{View, Text} from 'react-native';
-import Map from './MapComponents/Map'
+
 
 export default function MapScreen({navigation}) {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'white'}}>
       <Text onPress={() => navigation.navigate('Profile')}
       >MapScreen</Text>
-      <Map/>
-
     </View>
 
     );
