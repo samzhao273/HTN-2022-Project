5ec32c79f742c344cc5004f9457f6694f37e3438
samzhao273 <samzhao273@gmail.com>
2022-09-17 03:02:12 -0400

    added posting in general components

---
+++ frontend/MainApp/screens/GeneralComponents/Posting.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const Posting = () => {
+
+    return(
+        <View>
+            <Text>Poop</Text>
+        </View>
+
+    );
+}
+
+
+export default Posting;
+
--- frontend/MainApp/screens/ProfileComponents/Edit.js
+++ frontend/MainApp/screens/ProfileComponents/Edit.js
@@ -10,7 +10,6 @@ const Edit = () => {
             </View>
 
 
-
         </TouchableHighlight>
 
     )
