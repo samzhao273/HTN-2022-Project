13245c79ac24b4f46aebfa2a429aebea98a35ac7
samzhao273 <samzhao273@gmail.com>
2022-09-17 02:09:38 -0400

    changed name of navigation

--- frontend/App.js
+++ frontend/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Ionic from "react-native-vector-icons/Ionicons"
 import * as React from 'react';
-import MainContainer from './Navigation/MainContainer'
+import MainContainer from './MainApp/MainContainer'
 
 export default function App() {
   return (
