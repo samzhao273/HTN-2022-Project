826e73e518990246ec65861143d7d4e8102585a5
joanne <jlee2002@students.cs.ubc.ca>
2022-09-17 16:53:47 -0400

    posting container alignment, added posting button

--- frontend/MainApp/screens/ProfileComponents/Add.js
+++ frontend/MainApp/screens/ProfileComponents/Add.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image ,TextInput, TouchableOpacity, TouchableHighlight} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
-const Edit = () => {
+const Add = () => {
     return(
-        <TouchableHighlight onPress = {() => {alert('this is going to the edit screen')}}>
+        <TouchableHighlight onPress = {() => {alert('this is going to the add screen')}}>
             <View>
-                <Ionicons name = "pencil" size={40} color='#55B3AE'></Ionicons>
+                <Ionicons name = "md-add-circle" size={40} color='#55B3AE'></Ionicons>
             </View>
         </TouchableHighlight>
 
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
     
 })
 
-export default Edit;
+export default Add;
