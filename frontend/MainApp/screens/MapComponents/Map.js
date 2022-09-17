38f54837164bf20f4f5df5c89b0603da07d7115f
Aaron Chan <chan.aaron73@gmail.com>
2022-09-17 02:14:35 -0400

    map styling

--- frontend/MainApp/screens/MapComponents/Map.js
+++ frontend/MainApp/screens/MapComponents/Map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import Geolocation from "react-native-geolocation-service";
 
@@ -35,10 +35,22 @@ const Map = () => {
   }, []);
 
   return (
-    <View>
-      <MapView region={region}>{current}</MapView>
+    <View style={styles.map_container}>
+      <MapView region={region} style={styles.map}>{current}</MapView>
     </View>
   );
 };
 
 export default Map;
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  map_container: {
+    height: 700,
+    width: 400,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+});
