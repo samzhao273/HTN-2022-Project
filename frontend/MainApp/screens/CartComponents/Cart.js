3361fff0be6e8501d1e224fa4d88c708aa0883b0
Aaron Chan <chan.aaron73@gmail.com>
2022-09-17 22:50:08 -0400

    cart frontend

--- frontend/MainApp/screens/CartComponents/Cart.js
+++ frontend/MainApp/screens/CartComponents/Cart.js
@@ -3,7 +3,10 @@ import { View, Text, StyleSheet } from "react-native";
 const Cart = ({ type, brand, size, gender, tags, caption }) => {
   return (
     <View>
+      <Text style={styles.Line}>_____________________________________</Text>
+      <Text>{'\n'}</Text>
       <Text style={styles.Text}>{type}</Text>
+      <Text>{'\n'}</Text>
     </View>
   );
 };
@@ -13,6 +16,9 @@ export default Cart;
 const styles = StyleSheet.create({
   Text: {
     color: "#55B3AE",
-    fontSize: 30
+    fontSize: 30,
+  },
+  Line: {
+    color: "#55B3AE",
   },
 });
